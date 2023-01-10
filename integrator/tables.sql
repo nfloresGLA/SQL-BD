4e5e3b3fecf76cee7e3d3bb7b2c8ccb475ec18d3
nfloresGLA <nflores@andreani.com>
2023-01-10 18:15:04 -0300

    add tables stage-01

--- integrator/tables.sql
+++ integrator/tables.sql
@@ -1,3 +1,40 @@
 CREATE SCHEMA laboratorio;
-
 USE `laboratorio`;
+
+CREATE TABLE IF NOT EXISTS `laboratorio`.`Facturas` (
+	Letra char not null,
+	Numero int not null,
+	ClienteID int,
+	ArticuloID int,
+	Fecha date,
+	Monto double,
+	PRIMARY KEY (Letra, Numero)
+) 
+ENGINE = InnoDB; # Motor de almacenamiento transaccional (ACID)
+
+CREATE TABLE IF NOT EXISTS `laboratorio`.`Articulos` (
+	ArticuloID int not null auto_increment,
+	Nombre varchar(25),
+	Precio double,
+	Stock int,
+	PRIMARY KEY (articuloID)
+)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `laboratorio`.`Clientes` (
+	ClienteID int not null auto_increment,
+	Nombre varchar(25),
+	Apellido varchar(25),
+	CUIT varchar(16) unique,
+	Direccion varchar(50),
+	Comentarios varchar(200),
+	PRIMARY KEY (ClienteID)
+)
+ENGINE = InnoDB;
+
+# show databases;
+# show tables;
+
+# DESCRIBE Clientes;
+# DESCRIBE Articulos;
+# DESCRIBE Facturas;
