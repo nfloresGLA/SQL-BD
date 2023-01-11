296f348a040e112e6f540132594a94871930d949
nfloresGLA <nflores@andreani.com>
2023-01-11 15:13:26 -0300

    add tables and their modification stage-01

--- integrator/tables.sql
+++ integrator/tables.sql
@@ -42,3 +42,54 @@ ENGINE = InnoDB;
 # drop table `laboratorio`.`Facturas`;
 # drop table `laboratorio`.`Articulos`;
 # drop table `laboratorio`.`Clientes`;
+
+# ------------------------------ MODULO 2 ------------------------------ #
+
+SELECT * FROM clientes_neptuno; # IDCliente, NombreCompania, Direccion, Ciudad, Pais
+
+DESCRIBE clientes_neptuno;
+
+ALTER TABLE clientes_neptuno
+MODIFY COLUMN IDCliente varchar(5) PRIMARY KEY, # Modify = Modifica solamente el type o los parameters
+MODIFY COLUMN NombreCompania varchar(100) NOT NULL,
+MODIFY COLUMN Pais varchar(15) NOT NULL;
+
+ALTER TABLE clientes RENAME Contactos;
+
+SELECT * FROM contactos;
+
+SELECT * FROM clientes; # COD_CLIENTE, EMPRESA, DIRECCION, CIUDAD, TELEFONO, RESPONSABLE
+
+ALTER TABLE clientes
+MODIFY COLUMN COD_CLIENTE varchar(7) PRIMARY KEY, # Modify = Modifica solamente el type o los parameters
+MODIFY COLUMN EMPRESA varchar(100) NOT NULL,
+MODIFY COLUMN CIUDAD varchar(25) NOT NULL,
+MODIFY COLUMN TELEFONO INT UNSIGNED,
+MODIFY COLUMN RESPONSABLE varchar(30);
+
+DESCRIBE clientes;
+
+SELECT * FROM pedidos; # NUMERO_PEDIDO, CODIGO_CLIENTE, FECHA_PEDIDO, FORMA_PAGO, ENVIADO
+
+DESCRIBE pedidos;
+
+ALTER TABLE pedidos
+MODIFY COLUMN NUMERO_PEDIDO INT PRIMARY KEY, # Modify = Modifica solamente el type o los parameters
+MODIFY COLUMN CODIGO_CLIENTE varchar(7) NOT NULL,
+MODIFY COLUMN FECHA_PEDIDO DATE NOT NULL,
+MODIFY COLUMN FORMA_PAGO ENUM("APLAZADO", "CONTADO", "TARJETA"),
+MODIFY COLUMN ENVIADO ENUM ("SI", "NO");
+
+SELECT * FROM productos; # COD_PRODUCTO, SECCION, NOMBRE, IMPORTADO, ORIGEN, DIA, MES, ANO
+
+DESCRIBE productos;
+
+ALTER TABLE productos
+MODIFY COLUMN COD_PRODUCTO INT PRIMARY KEY, # Modify = Modifica solamente el type o los parameters
+MODIFY COLUMN SECCION varchar(20) NOT NULL,
+MODIFY COLUMN NOMBRE varchar(40) NOT NULL,
+MODIFY COLUMN IMPORTADO ENUM("VERDADERO", "FALSO"),
+MODIFY COLUMN ORIGEN varchar(25),
+MODIFY COLUMN DIA int UNSIGNED NOT NULL,
+MODIFY COLUMN MES int UNSIGNED NOT NULL,
+MODIFY COLUMN ANO int UNSIGNED NOT NULL;
